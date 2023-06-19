35ff95fb23352cc8d3e4c2873c74224f472c3783
Nihalahmed01 <137046902+Nihalahmed01@users.noreply.github.com>
2023-06-19 22:33:47 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,106 @@
+public class Data{
+	public static void main(String[] args){
+		byte MAX_VALUE = 127;
+		byte temperature = -124;
+		byte secondsInOneMinute = 60;
+		byte age = 22;
+		byte oneCentury = 100;
+		byte jersyNumberOFDhoni = 7;
+		byte freezingTemperature = -100;
+		byte boilingpoint = 100;
+		byte daysInAWeek = 7;
+		byte sundaysInAOneYear = 50;
+		System.out.println(MAX_VALUE);
+		System.out.println(temperature);
+		System.out.println(secondsInOneMinute);
+		System.out.println(age);
+		System.out.println(oneCentury);
+		System.out.println(jersyNumberOFDhoni);
+		System.out.println(freezingTemperature);
+		System.out.println(boilingpoint);
+		System.out.println(daysInAWeek);
+		System.out.println(sundaysInAOneYear);
+		
+		short MaxValuePositiveValue = 32767;	
+		short numberOfDaysInAYear = 365;
+		short distanceFromKashmirToKannyakumari = 3000;
+		short heightOfGautamBuddha = 182;
+		short minimumValue = 3268;
+		short populationInTown = 30000;
+		short annualIncome = 32000;
+		short ancientArtifactDates = 1000;
+		short odometreOfCar = 25000;
+		System.out.println(MaxValuePositiveValue);
+		System.out.println(numberOfDaysInAYear);
+		System.out.println(distanceFromKashmirToKannyakumari);
+		System.out.println(heightOfGautamBuddha);
+		System.out.println(minimumValue);
+		System.out.println(populationInTown);
+		System.out.println(annualIncome);
+		System.out.println(ancientArtifactDates);
+		System.out.println(odometreOfCar);
+		
+		int maxValue = 2000000000;
+		int populationOfBelgavi = 767000;
+		int dailyWebsiteTraffic = 1000000;
+		int maxStockPriceOfCompany = 60000;
+		int priceOfIphone14proMax = 150000;
+		int priceOfNinjaH2 = 2500000;
+		int phoneNumber = 725927871;
+		int landlineNumber = 1800568974;
+		int pinCodeNumber = 590010;
+		System.out.println(maxValue);
+		System.out.println(populationOfBelgavi);
+		System.out.println(dailyWebsiteTraffic);
+		System.out.println(maxStockPriceOfCompany);
+		System.out.println(priceOfIphone14proMax);
+		System.out.println(priceOfNinjaH2);
+		System.out.println(phoneNumber);
+		System.out.println(landlineNumber);
+		System.out.println(pinCodeNumber);
+		
+		long accNumber = 1234567891011L;
+		long bgmiId = 5647895216547L;
+		long USN = 247856321478L;
+		long IFSCCode = 2033654795440L;
+		long movieWithDurationOfmiliSeconds = 9000000000L;
+		long auctionPriceOfIshanKishan = 1500000000L;
+		long maximumValue = 2223372036854775807L;
+		System.out.println(accNumber);
+		System.out.println(bgmiId);
+		System.out.println(USN);
+		System.out.println(IFSCCode);
+		System.out.println(movieWithDurationOfmiliSeconds);
+		System.out.println(auctionPriceOfIshanKishan);
+		System.out.println(maximumValue);
+				
+		
+		float maximum = 3.25465897F;
+		float valueOfGamma = 0.7899964F;
+		float valueOfPi = 1.5432F;
+		float fuelPrice = 101.25F;
+		float caloriesBurnt = 120.2365F;
+		float weight = 60.23F;
+		float temperatureInside = 27.23F;
+		float freeStyleRecordOfSwimming = 52.35F;
+		float gasMileage = 7.5F;
+		float pHValueOfWaterater = 6.88F;
+		System.out.println(maximum);
+		System.out.println(valueOfGamma);
+		System.out.println(valueOfPi);
+		System.out.println(fuelPrice);
+		System.out.println(caloriesBurnt);
+		System.out.println(weight);
+		System.out.println(temperatureInside);
+		System.out.println(freeStyleRecordOfSwimming);
+		System.out.println(gasMileage);
+		System.out.println(pHValueOfWaterater);
+		
+		
+		
+		
+		
+		
+
+	}
+}
